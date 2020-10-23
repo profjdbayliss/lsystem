68e30b2a993dc17c00541cdc6475d488a150cc4e
jdbics <jdbics@rit.edu>
2020-10-23 15:30:19 -0400

    Removed Line Renderer
    Added OpenGL commands rather than line renderer use.

--- lsysExample2D/Assets/Scripts/LSystemController.cs
+++ lsysExample2D/Assets/Scripts/LSystemController.cs
@@ -8,6 +8,7 @@ using System.Text;
 public class LSystemController : MonoBehaviour {
 
     Hashtable ruleHash = new Hashtable(100);
+
 	public float initial_length = 2;
 	public float initial_radius = 1.0f;
 	StringBuilder start = new StringBuilder("");
@@ -19,11 +20,16 @@ public class LSystemController : MonoBehaviour {
 	public int iterations = 4;
 	
 	// for drawing lines
-	public Material lineMaterial; 
 	public float lineWidth = 1.0f;
+    List<List<Vector3>> allPositions = new List<List<Vector3>>(100);
+    List<Color> colors = new List<Color>(100);
 
-	void Start () {
+    static Material lineMaterial;
 
+    void Start () {
+
+        // create the line material for 2d openGL drawing
+        CreateLineMaterial();
 
         //variables : 0, 1
         //constants: [, ]
@@ -52,14 +58,7 @@ public class LSystemController : MonoBehaviour {
     }
 
     // Get a rule from a given letter that's in our array
-    // Assume that there's a 2d array of [letters, rules]
-    string getRule( string input) {
-		//for(int i=0; i<rules.GetLength(0); i++) {
-		//	//print (rules[i,0]);
-		//	if (rules[i,0].ToString().Equals( input )) {
-		//		return rules[i,1].ToString();
-		//	}
-		//}
+    string getRule( string input) {		
 		if (ruleHash.ContainsKey(input))
         {
             return (string)ruleHash[input];
@@ -82,32 +81,83 @@ public class LSystemController : MonoBehaviour {
 
     	lang = curr;
 	}
-	
-	// The display routine for the weed type plant above
-	void display3() {
+
+    // based on example at: https://docs.unity3d.com/ScriptReference/GL.html
+    static void CreateLineMaterial()
+    {
+        if (!lineMaterial)
+        {
+            // Unity has a built-in shader that is useful for drawing
+            // simple colored things.
+            Shader shader = Shader.Find("Hidden/Internal-Colored");
+            lineMaterial = new Material(shader);
+            lineMaterial.hideFlags = HideFlags.HideAndDontSave;
+            // Turn on alpha blending
+            lineMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+            lineMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+            // Turn backface culling off
+            lineMaterial.SetInt("_Cull", (int)UnityEngine.Rendering.CullMode.Off);
+            // Turn off depth writes
+            lineMaterial.SetInt("_ZWrite", 0);
+        }
+    }
+
+    // based on example at: https://docs.unity3d.com/ScriptReference/GL.html
+    public void OnRenderObject()
+    {
+        // Apply the line material
+        lineMaterial.SetPass(0);
+
+        GL.PushMatrix();
+        // Set transformation matrix for drawing to
+        // match our transform
+        GL.MultMatrix(transform.localToWorldMatrix);
+
+        // Draw lines
+        GL.Begin(GL.LINES);
+        RenderLineList(allPositions, colors);
+        GL.End();
+        GL.PopMatrix();
+    }
+
+    private void RenderLineList(List<List<Vector3>> lineList, List<Color> colors)
+    {
+        
+        int lineListCount = lineList.Count;
+
+        for (int j = 0; j < lineListCount; j++)
+        {
+            List<Vector3> line = lineList[j];
+            int lineCount = line.Count;
+            GL.Color(colors[j]);
+            for (int i = 0; i < lineCount-1; i++)
+            {
+                GL.Vertex(line[i]);
+                GL.Vertex(line[i + 1]);
+            }
+        }
+    }
+
+    // The display routine for the weed type plant above
+    void display3() {
 		
 		// to push and pop location and angles
-		Stack<float> positions = new Stack<float>();
-		Stack<float> angles = new Stack<float>();
-		
+		Stack<float> positions = new Stack<float>(10);
+		Stack<float> angles = new Stack<float>(10);
+
 		// current location and angle
 		float angle = 0f;
 		Vector3 position = new Vector3(0,0,0);
 		float posy = 0.0f;
 		float posx = 0.0f;
-		
-		// location and rotation to draw towards
+
+        // location and rotation to draw towards
 		Vector3 newPosition;
 		Vector2 rotated;
-		
+        allPositions.Clear();
+        List<Vector3> currentLine = new List<Vector3>(10);
+
 		// start at 0,0,0
-		// create a new object for every line segment
-		// Yes, this is hokey, but it's the easiest way to do things
-		contents = new GameObject();
-		GameObject tmp = contents;
-		LineRenderer line = tmp.AddComponent<LineRenderer>(); 
-		line.SetColors(Color.green,Color.green);
-		line.material = lineMaterial;
 
 		// Apply all the drawing rules to the lsystem string
 		for(int i=0; i<lang.Length; i++) {
@@ -127,13 +177,14 @@ public class LSystemController : MonoBehaviour {
 				newPosition = new Vector3(position.x, posy, 0);
 				rotated = rotate (position, new Vector3(position.x,posy,0), angle);
 				newPosition = new Vector3(rotated.x,rotated.y,0);
-				drawLSystemLine(position, newPosition, line, Color.green);
+                currentLine.Add(position);
+                currentLine.Add(newPosition);
+      
 				// set up for the next draw
 				position = newPosition;
 				posx = newPosition.x;
 				posy = newPosition.y;
-				tmp = new GameObject();
-				line = tmp.AddComponent<LineRenderer>(); 
+				
 				break;
 			case "[" :
 				//[: push position and angle
@@ -147,120 +198,120 @@ public class LSystemController : MonoBehaviour {
 				posx = positions.Pop();
 				posy = positions.Pop();
 				position = new Vector3(posx, posy, 0);
-				angle = angles.Pop();								
-				break;
-			default : break;
-			}
-			
-			
-		}
-	}
-	
-	// Display routine for 2nd example on the Wikipedia page
-	void display2() {
-		
-		// to push and pop location and angle
-		Stack<float> positions = new Stack<float>();
-		Stack<float> angles = new Stack<float>();
-		
-		// current angle and position
-		float angle = 0f;
-		Vector3 position = new Vector3(0,0,0);
-		float posy = 0.0f;
-		float posx = 0.0f;
-
-		// positions to draw towards
-		Vector3 newPosition;
-		Vector2 rotated;
-		
-		// start at 0,0,0
-		// create a new object for every line segment
-		contents = new GameObject();
-		GameObject tmp = contents;
-		LineRenderer line = tmp.AddComponent<LineRenderer>(); 
-
-		// Apply the drawing rules to the string given to us
-		for(int i=0; i<lang.Length; i++) {
-			string buff = lang[i].ToString();
-			switch (buff) {
-			case "0" : 
-				// draw a line ending in a leaf
-				posy += initial_length;
-				newPosition = new Vector3(position.x, posy, 0);
-				rotated = rotate (position, new Vector3(position.x,posy,0), angle);
-				newPosition = new Vector3(rotated.x,rotated.y,0);
-				drawLSystemLine(position, new Vector3(rotated.x,rotated.y,0), line, Color.red);
-				// set up for the next draw
-				position = newPosition;
-				posx = newPosition.x;
-				posy = newPosition.y;
-				tmp = new GameObject();
-				line = tmp.AddComponent<LineRenderer>(); 
-				drawCircle(0.05f, 0.05f, position, line, Color.blue);
-				tmp = new GameObject();
-				line = tmp.AddComponent<LineRenderer>(); 
-				break;
-			case "1" : 
-				// draw a line 
-				posy += initial_length;
-				newPosition = new Vector3(position.x, posy, 0);
-				rotated = rotate (position, new Vector3(position.x,posy,0), angle);
-				newPosition = new Vector3(rotated.x,rotated.y,0);
-				drawLSystemLine(position, newPosition, line, Color.green);
-				// set up for the next draw
-				position = newPosition;
-				posx = newPosition.x;
-				posy = newPosition.y;
-				tmp = new GameObject();
-				line = tmp.AddComponent<LineRenderer>(); 
-				break;
-			case "[" :
-				//[: push position and angle, turn left 45 degrees
-				positions.Push (posy);
-				positions.Push (posx);
-				float currentAngle = angle;
-				angles.Push(currentAngle);
-				angle -= 45;
-				break;
-			case "]" : 
-				//]: pop position and angle, turn right 45 degrees
-				posx = positions.Pop();
-				posy = positions.Pop();
-				position = new Vector3(posx, posy, 0);
 				angle = angles.Pop();
-				angle += 45;		
-				break;
+                // every time we pop we need to create
+                // a new line segment to draw
+                allPositions.Add(currentLine);
+                colors.Add(Color.blue);
+                currentLine = new List<Vector3>(10);                   
+                break;
 			default : break;
 			}
-			
-			
-		}
-	}
-	
-	// Draw a line with the given parameters
-	void drawLSystemLine (Vector3 from, Vector3 to, LineRenderer line, Color color){
-		line.SetColors(color,color);
-		line.material = lineMaterial;
-		float wide = lineWidth;
-		List<Vector3> drawArray = new List<Vector3>();
-		drawArray.Add(from);
-		drawArray.Add(to);
-		
-		line.SetWidth(wide, wide);
-		line.SetVertexCount(drawArray.Count);
-		
-		for (int i = 0; i < drawArray.Count; i++){
-			for (int j = i; j < drawArray.Count; j++){
-					line.SetPosition(j, drawArray[i]); //set the position of all unused points to the current point (to avoid weird stretching thing)
-			}
-				
-			line.SetPosition(i, drawArray[i]);
-		}
-	}
-	
-	// rotate a line and return the position after rotation
-	// Assumes rotation around the Z axis
-	Vector2 rotate(Vector3 pivotPoint, Vector3 pointToRotate, float angle) {
+            if (currentLine.Count > 0)
+            {
+                allPositions.Add(currentLine);
+                colors.Add(Color.green);
+            }
+
+        }
+        
+    }
+
+    // Display routine for 2d examples in the main program
+    void display2()
+    {
+
+        // to push and pop location and angle
+        Stack<float> positions = new Stack<float>();
+        Stack<float> angles = new Stack<float>();
+
+        // current angle and position
+        float angle = 0f;
+        Vector3 position = new Vector3(0, 0, 0);
+        float posy = 0.0f;
+        float posx = 0.0f;
+
+        // positions to draw towards
+        Vector3 newPosition;
+        Vector2 rotated;
+
+        // start at 0,0,0
+        // create a new object for every line segment
+        List<Vector3> currentLine = new List<Vector3>(10);
+        List<Vector3> leaf = new List<Vector3>(10);
+
+        // Apply the drawing rules to the string given to us
+        for (int i = 0; i < lang.Length; i++)
+        {
+            string buff = lang[i].ToString();
+            switch (buff)
+            {
+                case "0":
+                    // draw a line ending in a leaf
+                    posy += initial_length;
+                    newPosition = new Vector3(position.x, posy, 0);
+                    rotated = rotate(position, new Vector3(position.x, posy, 0), angle);
+                    newPosition = new Vector3(rotated.x, rotated.y, 0);
+                    currentLine.Add(position);
+                    currentLine.Add(new Vector3(rotated.x, rotated.y, 0));
+                    // set up for the next draw
+                    position = newPosition;
+                    posx = newPosition.x;
+                    posy = newPosition.y;
+                    drawCircle(0.45f, 0.45f, position, leaf);
+                    allPositions.Add(leaf);
+                    leaf = new List<Vector3>(10);
+                    colors.Add(Color.magenta);
+                    break;
+                case "1":
+                    // draw a line 
+                    posy += initial_length;
+                    newPosition = new Vector3(position.x, posy, 0);
+                    rotated = rotate(position, new Vector3(position.x, posy, 0), angle);
+                    newPosition = new Vector3(rotated.x, rotated.y, 0);
+                    currentLine.Add(position);
+                    currentLine.Add(newPosition);
+                    //drawLSystemLine(position, newPosition, line, Color.green);
+                    // set up for the next draw
+                    position = newPosition;
+                    posx = newPosition.x;
+                    posy = newPosition.y;
+                    //tmp = new GameObject();
+                    //line = tmp.AddComponent<LineRenderer>();
+                    break;
+                case "[":
+                    //[: push position and angle, turn left 45 degrees
+                    positions.Push(posy);
+                    positions.Push(posx);
+                    float currentAngle = angle;
+                    angles.Push(currentAngle);
+                    angle -= 45;
+                    break;
+                case "]":
+                    //]: pop position and angle, turn right 45 degrees
+                    posx = positions.Pop();
+                    posy = positions.Pop();
+                    position = new Vector3(posx, posy, 0);
+                    angle = angles.Pop();
+                    angle += 45;
+                    allPositions.Add(currentLine);
+                    colors.Add(Color.green);
+                    currentLine = new List<Vector3>(10);
+                    break;
+                default: break;
+            }
+            if (currentLine.Count > 0)
+            {
+                allPositions.Add(currentLine);
+                colors.Add(Color.green);
+            }
+
+        }
+    }
+
+    // rotate a line and return the position after rotation
+    // Assumes rotation around the Z axis
+    Vector2 rotate(Vector3 pivotPoint, Vector3 pointToRotate, float angle) {
    		Vector2 result;
    		float Nx = (pointToRotate.x - pivotPoint.x);
    		float Ny = (pointToRotate.y - pivotPoint.y);
@@ -272,25 +323,20 @@ public class LSystemController : MonoBehaviour {
 
 	// Draw a circle with the given parameters
 	// Should probably use different stuff than the default
-    void drawCircle(float radiusX, float radiusY, Vector3 center, LineRenderer line, Color color) {
+    void drawCircle(float radiusX, float radiusY, Vector3 center, List<Vector3> line) {
 
         float x;
         float y;
         float z = 0f;
 		int segments = 15;
         float angle = (360f / segments);
-			
-		line.SetVertexCount (segments + 1);       
-		line.material = lineMaterial;
-		line.SetColors(color,color);
 
         for (int i = 0; i < (segments + 1); i++) {
 
             x = Mathf.Sin (Mathf.Deg2Rad * angle) * radiusX + center.x;
             y = Mathf.Cos (Mathf.Deg2Rad * angle) * radiusY + center.y;
 
-            line.SetPosition (i,new Vector3(x,y,0) );
-
+            line.Add(new Vector3(x, y, 0));
             angle += (360f / segments);
 
         }
